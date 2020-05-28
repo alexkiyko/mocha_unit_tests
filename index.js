927921ce17a10726c9312377c03a5067866f41e6
Alexander Kiyko <55935063+alexkiyko@users.noreply.github.com>
2020-05-28 00:35:18 -0400

    add func removeDuplicates

--- index.js
+++ index.js
@@ -2,47 +2,74 @@ function sum(a = 0, b = 0) {
   return a + b;
 }
 
-function mult(a = 0, b = 0) {
+
+function mult(a, b) {
   return a * b;
 }
 
-function isPalindrome(str = '') {
+
+function isPalindrome(str) {
   return str.split('').reverse().join('').toLowerCase() === str.toLowerCase();
 }
+
 
 function sortArrayUp(arr = []) {
   return arr.sort((a, b) => a - b);
 }
 
+
 function oddOrEven(n) {
   return n % 2 === 0 ? 'Even' : 'Odd';
 }
 
-function makeUpperCase(str = '') {
+
+function makeUpperCase(str) {
   return str.toUpperCase(str);
 }
 
+
 function reversedString(str) {
   let rowBack = '';
-  for (let i = str.length -1; i >= 0; i--) {
-    if (str[i] !== ' '){
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] !== ' ') {
       rowBack += str[i];
     }
   }
   return rowBack;
 }
 
+
 function oppositeValue(number = 0) {
   return number * -1;
 }
 
+
 function typeOfSum(a, b) {
-  return typeof(a + b);
+  return typeof (a + b);
 }
 
-function stringClean(s){
+
+function stringClean(s) {
   let str = s.replace(/[0-9]/gi, '');
   return str;
 }
 
-module.exports = {sum, mult, isPalindrome, sortArrayUp, oddOrEven, makeUpperCase, reversedString, oppositeValue, typeOfSum, stringClean};
+
+function removeDuplicates(arr) {
+  return arr.filter((el, i) => i === arr.indexOf(el));
+}
+
+
+module.exports = {
+  sum,
+  mult,
+  isPalindrome,
+  sortArrayUp,
+  oddOrEven,
+  makeUpperCase,
+  reversedString,
+  oppositeValue,
+  typeOfSum,
+  stringClean,
+  removeDuplicates
+};
